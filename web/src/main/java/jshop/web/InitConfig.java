9fe5b05b4eea7c2073be7b9a8428e06f335749ee
jhkim31 <biz.jh.kim@gmail.com>
2024-08-06 17:31:48 +0900

    REFACTOR: 의존 정리 * web 모듈 불필요한 의존 정리 * core 모듈 불필요한 의존 정리 * build.gradle 공통 의존관계 정리

--- web/src/main/java/jshop/web/InitConfig.java
+++ web/src/main/java/jshop/web/InitConfig.java
@@ -2,11 +2,9 @@ package jshop.web;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @EnableRetry
-@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"jshop.common", "jshop.core"})
 public class InitConfig {}
