b0360bcfbb9d6b7c6ca7e29e61be2ec3f7a7db9a
jhkim31 <biz.jh.kim@gmail.com>
2024-08-22 21:36:28 +0900

    RELEASE: 0.1.6

---
+++ web/src/main/java/jshop/web/controller/TestController.java
@@ -0,0 +1,34 @@
+package jshop.web.controller;
+
+import jshop.core.domain.product.dto.SearchCondition;
+import jshop.core.domain.product.dto.SearchProductDetailsResponse;
+import jshop.core.domain.product.repository.SearchRepository;
+import jshop.core.domain.product.service.SearchService;
+import jshop.web.dto.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/auth/search")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final SearchService searchService;
+
+    @GetMapping
+    public Response<SearchProductDetailsResponse> searchProductDetail(SearchCondition condition,
+        @PageableDefault(size = 30) Pageable pageable) {
+        SearchProductDetailsResponse searchResult = searchService.search(condition, pageable);
+
+        return Response
+            .<SearchProductDetailsResponse>builder()
+            .data(searchResult)
+            .build();
+    }
+}
